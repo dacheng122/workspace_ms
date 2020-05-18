9830ec2f1d1edb4368645d2064d8b62685aca67e
满地节操12 <mguobiao@163.com>
2020-05-18 13:00:12 +0800

    11

---
+++ src/main/java/miaosha_idea/MainApplaction.java
@@ -0,0 +1,19 @@
+package miaosha_idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class MainApplaction {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplaction.class,args);
+
+
+    }
+
+
+}
