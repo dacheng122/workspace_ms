2c800e3c8d6acda24606d50e0f1f7ed3d23ca804
满地节操12 <mguobiao@163.com>
2020-05-21 18:09:05 +0800

    修改

--- src/main/java/miaosha_idea/MainApplaction.java
+++ src/main/java/miaosha_idea/MainApplaction.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class MainApplaction {
 
     public static void main(String[] args) {
-        //启动类
+        //启动类1
         SpringApplication.run(MainApplaction.class,args);
 
     }
